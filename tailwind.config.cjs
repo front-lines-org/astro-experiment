957b9d6cbf255c5c28538949955555e0f443b438
Ruben Soerdien <ruben.soerdien@hotmail.com>
2022-11-06 16:11:47 +0100

    add header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#f472b6",
+          secondary: "#34d399",
+          accent: "#60a5fa",
+          neutral: "#fbcfe8",
+          "base-100": "#ffffff",
+          info: "#95B8F9",
+          success: "#34d399",
+          warning: "#DFBD11",
+          error: "#F64C79",
+        },
+      },
+    ],
+  },
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     primary: "#f9a1bc",
@@ -13,5 +30,5 @@ module.exports = {
       body: ["'Libre Franklin', sans-serif"],
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
