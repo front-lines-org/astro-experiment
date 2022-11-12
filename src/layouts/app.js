c96523ce5d8bb5ad1d9eaa4372c286902dcee17b
Rubennaatje <ruben.soerdien@hotmail.com>
2022-11-12 23:07:05 +0100

    Fix swup

--- src/layouts/app.js
+++ src/layouts/app.js
@@ -3,5 +3,7 @@ import SwupHeadPlugin from "@swup/head-plugin";
 import SwupSlideTheme from "@swup/slide-theme";
 
 const swup = new Swup({
+  cache: true,
+  animateHistoryBrowsing: true,
   plugins: [new SwupHeadPlugin(), new SwupSlideTheme()],
 });
